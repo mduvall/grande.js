2df9a9fd3d45b39284aca4f517eb33d3ef4d5994
Matt DuVall <mduvall89@gmail.com>
2013-08-18 23:23:58 -0700

    Refactor has parentWithTag and onmouseup for document

--- js/grande.js
+++ js/grande.js
@@ -16,11 +16,18 @@
         len,
         node;
 
+    document.onmousedown = triggerTextSelection;
+    document.onmouseup = function(event) {
+      setTimeout(function() {
+        triggerTextSelection(event);
+      }, 1);
+    };
+
     for (i = 0, len = editableNodes.length; i < len; i++) {
       node = editableNodes[i];
       node.onmousedown = node.onkeyup = node.onmouseup = triggerTextSelection;
     }
-  };
+  }
 
   function iterateTextMenuButtons(callback) {
     var textMenuButtons = document.querySelectorAll(".g-body .text-menu button"),
@@ -35,15 +42,15 @@
         callback(n);
       })(node);
     }
-  };
+  }
 
   function bindTextStylingEvents() {
     iterateTextMenuButtons(function(node) {
       node.onmousedown = function(event) {
         triggerTextStyling(node);
-      }
-    })
-  };
+      };
+    });
+  }
 
   function reloadMenuState() {
     var className;
@@ -55,7 +62,7 @@
         case /bold/.test(className):
           // TODO: This is a funky case where contenteditable will hack the font-weight
           // instead...need to look out for that as well
-          if (hasParent(window.getSelection().focusNode, "b")) {
+          if (hasParentWithTag(window.getSelection().focusNode, "b")) {
             node.className = "bold active";
           } else {
             node.className = "bold";
@@ -63,7 +70,7 @@
           break;
 
         case /italic/.test(className):
-          if (hasParent(window.getSelection().focusNode, "i")) {
+          if (hasParentWithTag(window.getSelection().focusNode, "i")) {
             node.className = "italic active";
           } else {
             node.className = "italic";
@@ -71,7 +78,7 @@
           break;
 
         case /header1/.test(className):
-          if (hasParent(window.getSelection().focusNode, "h1")) {
+          if (hasParentWithTag(window.getSelection().focusNode, "h1")) {
             node.className = "header1 active";
           } else {
             node.className = "header1";
@@ -79,7 +86,7 @@
           break;
 
         case /header2/.test(className):
-          if (hasParent(window.getSelection().focusNode, "h2")) {
+          if (hasParentWithTag(window.getSelection().focusNode, "h2")) {
             node.className = "header2 active";
           } else {
             node.className = "header2";
@@ -87,7 +94,7 @@
           break;
 
         case /quote/.test(className):
-          if (hasParent(window.getSelection().focusNode, "blockquote")) {
+          if (hasParentWithTag(window.getSelection().focusNode, "blockquote")) {
             node.className = "blockquote active";
           } else {
             node.className = "blockquote";
@@ -101,7 +108,7 @@
           // no default
       }
     });
-  };
+  }
 
   function triggerTextStyling(node) {
     var className = node.className;
@@ -135,34 +142,42 @@
     }
 
     triggerTextSelection();
-  };
+  }
 
   function toggleFormatBlock(tag) {
-    if (hasParent(window.getSelection().focusNode, tag)) {
+    if (hasParentWithTag(window.getSelection().focusNode, tag)) {
       document.execCommand("formatBlock", false, "p");
       document.execCommand("outdent");
     } else {
       document.execCommand("formatBlock", false, tag);
     }
-  };
+  }
 
-  function hasParent(node, nodeType) {
+  function traverseParents(node, condition) {
     while (node.parentNode) {
-      if (node.nodeName.toLowerCase() === nodeType) {
+      if (condition(node)) {
         return true;
       }
       node = node.parentNode;
     }
 
     return false;
-  };
+  }
+
+  function hasParentWithTag(node, nodeType) {
+    return traverseParents(node,
+      function(n) {
+        return n.nodeName.toLowerCase() === nodeType;
+      }
+    );
+  }
 
   function triggerTextSelection() {
       var selectedText = root.getSelection(),
           range,
           clientRectBounds;
 
-      // The selection is collapsed, push the menu out of the way
+      // The selected text is collapsed, push the menu out of the way
       if (selectedText.isCollapsed) {
         setTextMenuPosition(-999, -999);
       } else {
@@ -177,12 +192,12 @@
         );
       }
 
-  };
+  }
 
   function setTextMenuPosition(top, left) {
     textMenu.style.top = top + "px";
     textMenu.style.left = left + "px";
-  };
+  }
 
   root.grande = grande;
 }).call(this);
