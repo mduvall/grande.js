6a768101fa295c9069fbdeb13e0b028f808e6582
Matt DuVall <mduvall89@gmail.com>
2013-09-01 22:48:51 -0700

    Add coverage for QUnit via Istanbul

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,21 @@
 module.exports = function(grunt) {
 
-  grunt.initConfig({});
+  grunt.initConfig({
+    qunit: {
+      options: {
+        "--web-security": "no",
+        coverage: {
+          src: ["js/grande.js"],
+          instrumentedFiles: "temp/",
+          htmlReport: "report/coverage",
+          coberturaReport: "report/",
+          linesThresholdPct: 85
+        }
+      },
+      all: ["test/**.html"]
+    }
+  });
+
+  grunt.loadNpmTasks("grunt-qunit-istanbul");
 };
 
