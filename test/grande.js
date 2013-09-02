621edc347ca65e06c670ae3885835719ae37d966
Matt DuVall <mduvall89@gmail.com>
2013-09-01 22:36:31 -0700

    Couple tests, include sinon

--- test/grande.js
+++ test/grande.js
@@ -1,5 +1,15 @@
-module("initialization", {});
+module("grande initialization");
 
-test("should be available at the global scope", function() {
+test("it should be available at the global scope", function() {
   ok(typeof window.grande === "object", "grande should be available at the window");
+});
+
+module("public API");
+
+test("it should provide bind as a method", function() {
+  ok(typeof window.grande.bind === "function", "bind should be a public API method");
+});
+
+test("it should provide select as a method", function() {
+  ok(typeof window.grande.select === "function", "select should be a public API method");
 });
