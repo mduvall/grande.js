d4299252fb429338a53bab9f49e28d3bb7499191
Knut Ole <knutolesjoli@gmail.com>
2014-09-25 05:15:46 +0200

    plugin: Attachments

--- js/grande.attachments.js
+++ js/grande.attachments.js
@@ -1,11 +1,15 @@
 // depends on grande.js (and grande.class.js)
-G.Plugins.Attachments = G.Rande.extend({
+G.Attachments = G.Class.extend({
+// G.Attachments = G.Class.extend({
 
 	name : 'Attachments',
 
-	initialize : function () {
-		console.log('initialize GrandeAttachments');
-		console.log('this: ', this);
+	initialize : function (source) {
+		this.source = source;
+		console.log('intilaisie: ', this.source, this);
+	},
+
+	_initialize : function () {
 
 		// create layout
 		this.initLayout();
@@ -15,104 +19,164 @@ G.Plugins.Attachments = G.Rande.extend({
 
 		// mark initialized
 		this.initialized = true;
+
+	},
+
+	
+
+	// hack cause i dont get it
+	plugin : function (grande) {
+
+		this.grande = grande;
+		this._initialize();
+
 	},
 
 	initLayout : function () {
 
-		// get grande containers
-		var toolbar = this._toolbarContainer = Grande.toolbarContainer;
-		// var ui = this._ui = toolbar.querySelectorAll('span.ui-inputs')[0];
-
 		// create button and append to ui container
-		var button = this._button = document.createElement('button');
+		this.button = this.createButton();
+		
+		// add to toolbar buttons container		
+		this.grande.addToolbarButton(this.button);
+
+	},
+
+	// createPopup : function () {
+	// 	this._container = document.createElement('div');
+	// 	this._inner = document.createElement('div');
+	// },
+
+	createButton : function () {
+		var button = document.createElement('button');
 		button.className = 'attachment';
 		button.innerHTML = 'F';
-		// ui.appendChild(button);
-
-		// attach to Grande
-		Grande.addToolbarButton(button);
-
-		// create filepane
-		this.filePane = {};
-		var container = this.filePane.container = document.createElement('div');
-		var inner = this.filePane.inner = document.createElement('div');
-
+		return button;
 	},
 
 	addHooks : function () {
 		var that = this;
-		this._button.addEventListener('mousedown', function (e) {
+		this.button.addEventListener('mousedown', function (e) {
 			that.toggleButton(e, that);
 		}, false);
 	},
 
 	removeHooks : function () {
 		var that = this;
-		this._button.removeEventListener('mousedown', function (e) {
+		this.button.removeEventListener('mousedown', function (e) {
 			that.toggleButton(e, that);
 		}, false);
 	},
 
 	toggleButton : function (e, that) {
+
 		var button = e.target;
 		Wu.DomEvent.stop(e);
 
 		if (button.active) {
-			that.closeFilepane();
+			that.closePopup();
 			button.active = false;
 			button.className = 'attachment';
 		} else {
-			that.openFilepane();
+			that.openPopup();
 			button.active = true;
 			button.className = 'attachment active';
 		}
 	},
 
-	openFilepane : function () {
+	closePopup : function () {
+		console.log('clsoePane!!!', this);
 
-		// get project
-		var project = app.activeProject;
+		// unlink
 
-		console.log('openFilepane');
-		console.log('this: ', this);
+
+		// remove popup
+		this.destroyPopup();
+
+	},
+
+	openPopup : function () {
 
 		// select text
-		this.selectText();
 		var selectedText = window.getSelection();
 		var range = selectedText.getRangeAt(0);
 		var clientRectBounds = range.getBoundingClientRect();
-		console.log('free! ', selectedText, range, clientRectBounds);
-
-		// this.Grande.setTextMenuPosition(
-		// 	clientRectBounds.top - 5 + window.pageYOffset,
-		// 	(clientRectBounds.left + clientRectBounds.right) / 2
-		// );
-
+		// console.log('free! ', selectedText, range, clientRectBounds);
 		
 		// create popup
-		this.createPopup(files);
-
-
-	},
-
-	createPopup : function (files) {
-
-		// get files
-		var files = project.getFiles();
-		console.log('files: ', files);
-
-
-		var container = Wu.DomUtil.create('div', 'grande-files-container');
-		var topwrapper = Wu.DomUtil.create('div', 'grande-files-topwrap');
-
-
-
-
+		this.createPopup();
 
 	},
 
-	closeFilepane : function () {
-		console.log('closeFilepane');
+	createPopup : function () {
+
+		// get project
+		this.project = app.activeProject;
+
+		// get sources
+		var sources = this.source;
+		// var collections = this.project.getCollections();
+
+		console.log('sources: ', sources);
+
+
+		var container = this._popup = Wu.DomUtil.create('div', 'grande-sources-container');
+		var topwrapper = Wu.DomUtil.create('div', 'grande-sources-topwrap', container);
+
+		sources.forEach(function (source) {
+			this._createSource(source, container);
+		}, this);
+
+		this.grande.addToOptions(container);
+
+	},
+
+
+	_createSource : function (source, container) {
+
+		var title = source.name;
+		var wrap = Wu.DomUtil.create('div', 'grande-sources-source', container);
+		var icon = Wu.DomUtil.create('div', 'grande-sources-source-icon', wrap);
+		var name = Wu.DomUtil.create('div', 'grande-sources-source-title', wrap, title);
+
+		Wu.DomUtil.addClass(icon, source.type);
+		
+		// if (source.type == 'image') {
+		// 	var size = '?width=50&height=50';
+		// 	var url = '/pixels/' + source.uuid + size;
+		// 	console.log('url: ', url);
+		// 	var thumb = Wu.DomUtil.create('img', 'grande-sources-source-thumb', wrap);
+		// 	thumb.src = url;
+		// 	console.log('thumb: ', thumb);
+		// } 
+
+		Wu.DomEvent.on(wrap, 'mousedown', function () {
+			this.selectSource(source);
+		}, this);
+	},
+
+	destroyPopup : function () {
+		this._remove(this._popup);
+		this._popup = null;		// events should now be gc'd. todo: check
+	},
+
+	selectSource : function (source) {
+		console.log('selected source!', source);
+
+		// what to do when source is selected
+		// add link go text selection
+		var url = source.uuid;
+
+		this.grande.createLink(url);
+
+		
+	},
+
+	_remove: function (el) {
+		var parent = el.parentNode;
+		if (parent) {
+		    parent.removeChild(el);
+		}
 	},
 
 
@@ -121,12 +185,6 @@ G.Plugins.Attachments = G.Rande.extend({
 		console.log('destroy');
 		this.removeHooks();
 	},
-
-	// register plugin
-	register : function () {
-		this.Grande = Grande.registerPlugin(this);
-		console.log('regitered: ', this.Grande);
-	}
 
 });
 
